2a16383a12e5303fc13a57061f1ea84611b9b7d3
ZeviGaming698 <44415682+ZeviGaming698@users.noreply.github.com>
2019-04-08 18:21:17 -0400

    Update and rename manyblocks.js to glowingobsidian.js

---
+++ glowingobsidian.js
@@ -0,0 +1,3 @@
+Block.defineBlock(180, "Glowing Obsidian", [["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0], ["glowing_obsidian", 0], []]);
+Block.setLightLevel(180, 15);
+Block.setDestroyTime(180, 90);
